15f4bd2b4bf0510bbe17622e53540c083407e48f
Kirill Ermakov <lvstrng161@gmail.com>
2025-02-08 13:47:18 +0400

    added github workflow

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
+import logging
+from contextlib import asynccontextmanager
+
 import dotenv
+
 
 dotenv.load_dotenv()
 
@@ -6,19 +10,23 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
 
-from routers import users, status
+from app.routers import users, status
 from app.db.engine import create_db_and_tables
 
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    logging.warning('On startup')
+    create_db_and_tables()
+    yield
+    logging.warning('On shutdown')
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(status.router)
 app.include_router(users.router)
 add_pagination(app)
 
 
-
-
 if __name__ == "__main__":
-    create_db_and_tables()
     uvicorn.run(app, host="127.0.0.1", port=8000)
